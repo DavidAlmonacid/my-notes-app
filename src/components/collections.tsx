337ad9fefb8d7e033b5a87e200271423c48efb38
David Almonacid <david.almonacidf@gmail.com>
2024-05-30 11:21:36 -0500

    chore: Refactor Collections component to remove unused imports and update JSX structure

--- src/components/collections.tsx
+++ src/components/collections.tsx
@@ -1,9 +1,5 @@
 import { createClient } from "edgedb";
-import { Plus } from "lucide-react";
-
 import e from "@root/dbschema/edgeql-js";
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
 
 const client = createClient();
 
@@ -16,20 +12,8 @@ export async function Collections() {
   const collections = await selectCollections.run(client);
 
   return (
-    <section className="w-64 border-r border-r-white/15 py-6 px-4">
-      <h2 className="flex justify-around py-4 text-lg font-medium">
-        <span>Collections</span>
-
-        <Button
-          variant="ghost"
-          size="icon"
-          className="border border-white rounded-md size-6"
-        >
-          <Plus width={20} />
-        </Button>
-      </h2>
-
-      {collections && collections.length === 0 ? (
+    <div className="py-4 text-sm">
+      {collections.length === 0 ? (
         <p>No collections found</p>
       ) : (
         <ul>
@@ -40,6 +24,6 @@ export async function Collections() {
           ))}
         </ul>
       )}
-    </section>
+    </div>
   );
 }
