b8f6a0246fcb3e3be89b8b8f75a69458661d42dc
David Almonacid <david.almonacidf@gmail.com>
2024-05-31 18:14:53 -0500

    feat: Add RenameInputCollection component for renaming collections

---
+++ src/components/rename-input-collection.tsx
@@ -0,0 +1,27 @@
+import { updateCollectionName } from "@/actions/collections-actions";
+import { Input } from "./ui/input";
+
+interface Props {
+  collection: {
+    id: string;
+    name: string;
+  };
+  endRename: () => void;
+}
+
+export function RenameInputCollection({ collection, endRename }: Props) {
+  return (
+    <form action={updateCollectionName} onSubmit={endRename}>
+      <input type="hidden" value={collection.id} name="collectionId" />
+      <Input
+        type="text"
+        className="h-fit p-0.5"
+        defaultValue={collection.name}
+        name="collectionName"
+        required
+        autoFocus
+        onBlur={endRename}
+      />
+    </form>
+  );
+}
