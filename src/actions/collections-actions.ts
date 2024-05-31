82a2d32c6a40da590a483d378f6327da315c3ee3
David Almonacid <david.almonacidf@gmail.com>
2024-05-31 18:17:30 -0500

    feat: Update collection name in updateCollectionName function

--- src/actions/collections-actions.ts
+++ src/actions/collections-actions.ts
@@ -22,3 +22,23 @@ export async function createCollection(formData: FormData) {
 
   revalidatePath("/");
 }
+
+export async function updateCollectionName(formData: FormData) {
+  const collectionName = formData.get("collectionName") as string;
+  const collectionId = formData.get("collectionId") as string;
+
+  if (!collectionName) {
+    return;
+  }
+
+  const query = e.update(e.Collection, () => ({
+    filter_single: { id: collectionId },
+    set: {
+      name: collectionName
+    }
+  }));
+
+  await query.run(client);
+
+  revalidatePath("/");
+}
