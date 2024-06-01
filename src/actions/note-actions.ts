375eed750ea36c3e66d9bc63a7babb857e706627
David Almonacid <david.almonacidf@gmail.com>
2024-06-01 12:13:17 -0500

    feat: Add createNote function to note-actions.ts

---
+++ src/actions/note-actions.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { createClient } from "edgedb";
+import { revalidatePath } from "next/cache";
+
+import e from "@root/dbschema/edgeql-js";
+
+const client = createClient();
+
+export async function createNote(formData: FormData) {
+  const noteTitle = formData.get("noteTitle") as string;
+  const collectionId = formData.get("collectionId") as string;
+
+  if (!noteTitle) {
+    return;
+  }
+
+  const query = e.insert(e.Note, {
+    title: noteTitle,
+    collection: e.select(e.Collection, () => ({
+      filter_single: { id: collectionId }
+    }))
+  });
+
+  await query.run(client);
+}
