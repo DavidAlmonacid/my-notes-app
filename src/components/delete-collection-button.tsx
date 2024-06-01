0f56375868c525052f3a404c715136e5c7391e44
David Almonacid <david.almonacidf@gmail.com>
2024-05-31 23:32:31 -0500

    feat: Add DeleteCollectionButton component for deleting collections

---
+++ src/components/delete-collection-button.tsx
@@ -0,0 +1,18 @@
+import { deleteCollection } from "@/actions/collections-actions";
+import { Button } from "./ui/button";
+
+interface Props {
+  collectionId: string;
+  children: React.ReactNode;
+}
+
+export function DeleteCollectionButton({ collectionId, children }: Props) {
+  return (
+    <form action={deleteCollection}>
+      <input type="hidden" name="collectionId" value={collectionId} />
+      <Button variant="destructive" size="sm" className="w-full" type="submit">
+        {children}
+      </Button>
+    </form>
+  );
+}
