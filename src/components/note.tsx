d78c99fe14a99153e4060a0c07bfbd924942f73f
David Almonacid <david.almonacidf@gmail.com>
2024-06-07 18:10:02 -0500

    chore: Remove unused code in Note component

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -53,32 +53,3 @@ export function Note({ note }: Props) {
     </ContextMenu>
   );
 }
-
-/* module default {
-  type Collection {
-    required name: str;
-    required created_at: datetime {
-      default := (datetime_current());
-      readonly := true;
-    };
-    required updated_at: datetime {
-      default := (datetime_current());
-    };
-  }
-
-  type Note {
-    required title: str;
-    required content: str {
-      default := "";
-    };
-    required created_at: datetime {
-      default := (datetime_current());
-      readonly := true;
-    };
-    required updated_at: datetime {
-      default := (datetime_current());
-    };
-    required collection_id: str;
-    required collection: Collection;
-  }
-} */
