77a3b0502fbc00e7764e03e1476c19ca3090ebaf
David Almonacid <david.almonacidf@gmail.com>
2024-06-11 12:49:37 -0500

    Refactor Collection component to use CollectionName component

--- src/components/collection.tsx
+++ src/components/collection.tsx
@@ -1,19 +1,10 @@
 "use client";
 
 import type { Collection } from "@prisma/client";
-import clsx from "clsx";
-import { ChevronRight, TextCursorInput, Trash } from "lucide-react";
 import { useRef, useState } from "react";
 
-import { DeleteCollectionButton } from "./delete-collection-button";
+import { CollectionName } from "./collection-name";
 import { RenameInputCollection } from "./rename-input-collection";
-import { Button } from "./ui/button";
-import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuTrigger
-} from "./ui/context-menu";
 
 interface Props {
   collection: Pick<Collection, "id" | "name">;
@@ -50,41 +41,11 @@ export function Collection({ collection, children }: Props) {
             summaryRef={summaryRef}
           />
         ) : (
-          <ContextMenu>
-            <ContextMenuTrigger>
-              <div className="flex items-center gap-x-0.5">
-                <ChevronRight
-                  className={clsx(
-                    "w-full max-w-5 h-5",
-                    isOpened && "rotate-90"
-                  )}
-                />
-                <span>{collection.name}</span>
-              </div>
-            </ContextMenuTrigger>
-
-            <ContextMenuContent className="flex flex-col gap-y-1.5 p-2 min-w-36">
-              <ContextMenuItem className="p-0">
-                <Button
-                  variant="ghost"
-                  size="sm"
-                  type="button"
-                  className="px-2 py-1.5 h-fit w-full justify-between"
-                  onClick={handleRenameCollection}
-                >
-                  <span>Rename</span>
-                  <TextCursorInput className="size-5" />
-                </Button>
-              </ContextMenuItem>
-
-              <ContextMenuItem className="p-0">
-                <DeleteCollectionButton collectionId={collection.id}>
-                  <span>Delete</span>
-                  <Trash className="size-5 p-[1px]" />
-                </DeleteCollectionButton>
-              </ContextMenuItem>
-            </ContextMenuContent>
-          </ContextMenu>
+          <CollectionName
+            collection={collection}
+            isOpened={isOpened}
+            handleRenameCollection={handleRenameCollection}
+          />
         )}
       </summary>
 
