a606b5185fc532b80fb3fafb900843910e329286
David Almonacid <david.almonacidf@gmail.com>
2024-06-13 09:52:20 -0500

    Refactor database functions

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,19 +1,21 @@
 import prisma from "@/lib/prisma";
 import type { Collection, PartialNote } from "@/types/interfaces";
 
-export async function getCollections(): Promise<Collection[]> {
-  return await prisma.collection.findMany({
+export async function getCollections() {
+  const collections: Collection[] = await prisma.collection.findMany({
     select: { id: true, name: true },
     orderBy: { createdAt: "asc" }
   });
+
+  return collections;
 }
 
-export async function getNotesByCollectionId(
-  collectionId: string
-): Promise<PartialNote[]> {
-  return await prisma.note.findMany({
+export async function getNotesByCollectionId(collectionId: string) {
+  const notes: PartialNote[] = await prisma.note.findMany({
     select: { id: true, title: true },
     orderBy: { createdAt: "asc" },
     where: { collectionId }
   });
+
+  return notes;
 }
