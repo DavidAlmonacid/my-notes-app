c0bc1bcfcb0b8cfec66f30511f8a1b8df71b9eb3
David Almonacid <david.almonacidf@gmail.com>
2024-06-13 18:38:56 -0500

    feat: Add delete confirmation dialog to CollectionName component

--- src/components/collection-name.tsx
+++ src/components/collection-name.tsx
@@ -1,9 +1,17 @@
 import clsx from "clsx";
 import { ChevronRight, TextCursorInput, Trash } from "lucide-react";
+import { useEffect, useState } from "react";
 
 import type { Collection } from "@/types/interfaces";
-import { DeleteCollectionButton } from "./delete-collection-button";
 import { Button } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "./ui/card";
 import {
   ContextMenu,
   ContextMenuContent,
@@ -22,38 +30,84 @@ export function CollectionName({
   isOpened,
   handleRenameCollection
 }: Props) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [collectionNotesLength, setCollectionNotesLength] = useState(0);
+
+  useEffect(() => {
+    fetch(`/api/collection-notes-count/${collection.id}`)
+      .then((response) => response.json())
+      .then(({ count }) => setCollectionNotesLength(count));
+  });
+
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   // setIsDialogOpen(true);
+
+  //   if (collectionNotesLength > 0) {
+  //     setIsDialogOpen(true);
+  //   }
+  // };
+
   return (
-    <ContextMenu>
-      <ContextMenuTrigger>
-        <div className="flex items-center gap-x-0.5">
-          <ChevronRight
-            className={clsx("w-full max-w-5 h-5", isOpened && "rotate-90")}
-          />
-          <span>{collection.name}</span>
-        </div>
-      </ContextMenuTrigger>
+    <>
+      <ContextMenu>
+        <ContextMenuTrigger>
+          <div className="flex items-center gap-x-0.5">
+            <ChevronRight
+              className={clsx("w-full max-w-5 h-5", isOpened && "rotate-90")}
+            />
+            <span>{collection.name}</span>
+          </div>
+        </ContextMenuTrigger>
 
-      <ContextMenuContent className="flex flex-col gap-y-1.5 p-2 min-w-36">
-        <ContextMenuItem className="p-0">
-          <Button
-            variant="ghost"
-            size="sm"
-            type="button"
-            className="px-2 py-1.5 h-fit w-full justify-between"
-            onClick={handleRenameCollection}
-          >
-            <span>Rename</span>
-            <TextCursorInput className="size-5" />
-          </Button>
-        </ContextMenuItem>
+        <ContextMenuContent className="flex flex-col gap-y-1.5 p-2 min-w-36">
+          <ContextMenuItem className="p-0">
+            <Button
+              variant="ghost"
+              size="sm"
+              type="button"
+              className="px-2 py-1.5 h-fit w-full justify-between"
+              onClick={handleRenameCollection}
+            >
+              <span>Rename</span>
+              <TextCursorInput className="size-5" />
+            </Button>
+          </ContextMenuItem>
 
-        <ContextMenuItem className="p-0">
-          <DeleteCollectionButton collectionId={collection.id}>
+          <ContextMenuItem className="p-0">
+            {/* <DeleteCollectionButton collectionId={collection.id}>
             <span>Delete</span>
             <Trash className="size-5 p-[1px]" />
-          </DeleteCollectionButton>
-        </ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+          </DeleteCollectionButton> */}
+            <Button onClick={() => setIsDialogOpen(true)}>
+              <span>Delete</span>
+              <Trash className="size-5 p-[1px]" />
+            </Button>
+          </ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
+
+      {isDialogOpen && (
+        <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+          <Card className="absolute w-[350px]">
+            <CardHeader>
+              <CardTitle>Create project</CardTitle>
+              <CardDescription>
+                Deploy your new project in one-click.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <span>Hola</span>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button variant="outline" className="pointer-events-auto">
+                Cancel
+              </Button>
+              <Button className="pointer-events-auto">Deploy</Button>
+            </CardFooter>
+          </Card>
+        </div>
+      )}
+    </>
   );
 }
