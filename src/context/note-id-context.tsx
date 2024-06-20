80034fd0392fa2d0db9c909320c50c423e8c3c63
David Almonacid <david.almonacidf@gmail.com>
2024-06-20 14:49:39 -0500

    feat: Update NoteIdContext to use null as initial value for noteId

--- src/context/note-id-context.tsx
+++ src/context/note-id-context.tsx
@@ -3,14 +3,14 @@
 import { createContext, useContext, useState } from "react";
 
 interface NoteIdContextType {
-  noteId?: string;
-  setNoteId: (noteId: string) => void;
+  noteId: string | null;
+  setNoteId: (noteId: string | null) => void;
 }
 
 export const NoteIdContext = createContext({} as NoteIdContextType);
 
 export function NoteIdProvider({ children }: { children: React.ReactNode }) {
-  const [noteId, setNoteId] = useState<string | undefined>(undefined);
+  const [noteId, setNoteId] = useState<string | null>(null);
 
   return (
     <NoteIdContext.Provider value={{ noteId, setNoteId }}>
