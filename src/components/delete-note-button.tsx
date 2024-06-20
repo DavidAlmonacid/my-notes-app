ba68642df77749245cded5b96dbf71a9b3ec1547
David Almonacid <david.almonacidf@gmail.com>
2024-06-20 15:05:26 -0500

    feat: Set note id to null when a note is deleted and it is the current displayed note

--- src/components/delete-note-button.tsx
+++ src/components/delete-note-button.tsx
@@ -1,4 +1,5 @@
 import { deleteNote } from "@/actions/note-actions";
+import { useNoteId } from "@/context/note-id-context";
 import { Button } from "./ui/button";
 
 interface Props {
@@ -8,6 +9,14 @@ interface Props {
 }
 
 export function DeleteNoteButton({ noteId, collectionId, children }: Props) {
+  const { noteId: currentNoteId, setNoteId } = useNoteId();
+
+  const handleClick = () => {
+    if (currentNoteId === noteId) {
+      setNoteId(null);
+    }
+  };
+
   return (
     <form action={deleteNote} className="w-full">
       <input type="hidden" name="noteId" value={noteId} />
@@ -16,6 +25,7 @@ export function DeleteNoteButton({ noteId, collectionId, children }: Props) {
         variant="destructive"
         size="sm"
         className="px-2 py-1.5 h-fit w-full justify-between bg-destructive/40 hover:bg-destructive/50"
+        onClick={handleClick}
         type="submit"
       >
         {children}
