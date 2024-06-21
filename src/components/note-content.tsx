1b609002faf1ae860a3da5effd4360fa9cb8f64f
David Almonacid <david.almonacidf@gmail.com>
2024-06-21 12:16:37 -0500

    feat: Update the note title when the input changes

--- src/components/note-content.tsx
+++ src/components/note-content.tsx
@@ -10,6 +10,8 @@ import { Textarea } from "./ui/textarea";
 
 export function NoteContent() {
   const [note, setNote] = useState<Note | null>(null);
+  const [noteTitle, setNoteTitle] = useState("");
+
   const { noteId } = useNoteId();
 
   useEffect(() => {
@@ -20,8 +22,23 @@ export function NoteContent() {
 
     fetch(`/api/get-note/${noteId}`)
       .then((response) => response.json())
-      .then(({ note }) => setNote(note));
+      .then(({ note }) => {
+        setNote(note);
+        setNoteTitle(note.title);
+      });
   }, [noteId]);
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNoteTitle(event.target.value);
+
+    fetch(`/api/update-note-title/${noteId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ title: event.target.value })
+    });
+  };
 
   return !note ? (
     <div className="grow px-10 py-5 font-mono text-center">
@@ -37,8 +54,9 @@ export function NoteContent() {
       <div>
         <Input
           aria-label="Note title"
-          value={note?.title}
+          value={noteTitle}
           className="text-xl border-0 focus-visible:ring-0"
+          onChange={handleTitleChange}
         />
         <hr />
         <span className="text-[13px] opacity-80 block mt-2">
